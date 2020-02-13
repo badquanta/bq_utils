2c034685fb47781eadb7b57f1f2b70cbb36aaf9f
BadQuanta <BadQuanta@gmail.com>
2020-02-13 00:34:50 -0800

    CMake first stabs...
    `lib/` is where to keep external dependencies.
    `scripts/` is where to keep development tools like bash/cmake scripts.
    `XXXXXXXXXXXX` marks the spot(s...) to rename.

---
+++ src/XXXXXXXXXXXX.cpp
@@ -0,0 +1,21 @@
+/*
+ * XXXXXXXXXXXX.cpp
+ *
+ *  Created on: Feb 13, 2020
+ *      Author: badquanta
+ */
+
+#include "XXXXXXXXXXXX.h"
+
+namespace bq {
+  
+  XXXXXXXXXXXX::XXXXXXXXXXXX() {
+    // TODO Auto-generated constructor stub
+    
+  }
+  
+  XXXXXXXXXXXX::~XXXXXXXXXXXX() {
+    // TODO Auto-generated destructor stub
+  }
+
+} /* namespace bq */
---
+++ src/XXXXXXXXXXXX_test.cpp
@@ -0,0 +1,19 @@
+/*
+ * XXXXXXXXXXXX_test.cpp
+ *
+ *  Created on: Feb 13, 2020
+ *      Author: badquanta
+ */
+
+#include "XXXXXXXXXXXX.h"
+#include "../util/bqdoctest.hpp"
+namespace bq {
+
+TEST_CASE("TODO: Window_test")
+{ 
+  CHECK(true)
+  ;
+}
+
+}
+/* namespace bq */
